fc68c79f5dd4940e7671f81deec0d679ebf8f76d
Lavcodes <sanjali_b@ce.iitr.ac.in>
2021-06-27 03:20:35 +0530

    gitignore node_modules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const pool = require('./database_config');
-
+const path = require('path');
 
 app.use(express.json());
 
@@ -109,6 +109,9 @@ if(process.env.NODE_ENV ==='production'){
 
 
 }
+app.get("*", (req, res) =>{
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+})
 
 const PORT = process.env.PORT || 5000;
 
